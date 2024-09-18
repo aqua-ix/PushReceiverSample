aaa259bd22a5ddc91202008e2dceae603b6f87ff
Soichi Ikebe <souichi1997@gmail.com>
2024-09-19 01:33:15 +0900

    Show last received time on screen

--- app/src/main/java/com/example/pushreceiversample/MainActivity.kt
+++ app/src/main/java/com/example/pushreceiversample/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.pushreceiversample
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -11,15 +13,24 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import com.example.pushreceiversample.ui.theme.PushReceiverSampleTheme
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.isGranted
 import com.google.accompanist.permissions.rememberPermissionState
 import com.google.accompanist.permissions.shouldShowRationale
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.collectLatest
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,6 +48,7 @@ class MainActivity : ComponentActivity() {
                         horizontalAlignment = Alignment.CenterHorizontally
                     ) {
                         RequestMmsPermission()
+                        ReceivedMessage()
                     }
                 }
             }
@@ -52,5 +64,40 @@ fun RequestMmsPermission() {
         mmsPermissionState.status.isGranted -> Text("MMS permission granted")
         mmsPermissionState.status.shouldShowRationale -> Text("MMS permission denied")
         else -> SideEffect { mmsPermissionState.launchPermissionRequest() }
+    }
+}
+
+@Composable
+fun ReceivedMessage() {
+    val context = LocalContext.current
+    var time by remember { mutableStateOf("") }
+
+    LaunchedEffect(Unit) {
+        listenToSharedPreferences(context).collectLatest { newMessage ->
+            time = newMessage
+        }
+    }
+
+    if (time.isNotEmpty()) {
+        Text(text = "MMS Message Received at: $time")
+    }
+}
+
+fun listenToSharedPreferences(context: Context) = callbackFlow {
+    val sharedPref = context.getSharedPreferences("PushMessages", Context.MODE_PRIVATE)
+    val listener = SharedPreferences.OnSharedPreferenceChangeListener { _, key ->
+        if (key == "lastReceived") {
+            val newMessage = sharedPref.getString(key, "") ?: ""
+            trySend(newMessage)
+        }
+    }
+
+    sharedPref.registerOnSharedPreferenceChangeListener(listener)
+
+    val initialMessage = sharedPref.getString("lastReceived", "") ?: ""
+    trySend(initialMessage)
+
+    awaitClose {
+        sharedPref.unregisterOnSharedPreferenceChangeListener(listener)
     }
 }
--- app/src/main/java/com/example/pushreceiversample/PushReceiver.kt
+++ app/src/main/java/com/example/pushreceiversample/PushReceiver.kt
@@ -5,11 +5,26 @@ import android.content.Context
 import android.content.Intent
 import android.provider.Telephony
 import android.util.Log
+import android.widget.Toast
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 class PushReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
         if (Telephony.Sms.Intents.WAP_PUSH_RECEIVED_ACTION == intent.action) {
             Log.d("PushReceiver", "WAP Push message received")
+            val currentTime = Date()
+            val formatter = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault())
+            val formattedTime = formatter.format(currentTime)
+
+            val sharedPref = context.getSharedPreferences("PushMessages", Context.MODE_PRIVATE)
+            with(sharedPref.edit()) {
+                putString("lastReceived", formattedTime)
+                apply()
+            }
+
+            Toast.makeText(context, "New WAP Push message received", Toast.LENGTH_SHORT).show()
         }
     }
 }
