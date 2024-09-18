a5da613a592c3adeecf78682ad6b974a00e77217
Soichi Ikebe <souichi1997@gmail.com>
2024-09-19 00:32:53 +0900

    Implement RECEIVE_MMS permission screen

--- app/src/main/java/com/example/pushreceiversample/MainActivity.kt
+++ app/src/main/java/com/example/pushreceiversample/MainActivity.kt
@@ -4,7 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.pushreceiversample.ui.theme.PushReceiverSampleTheme
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.isGranted
+import com.google.accompanist.permissions.rememberPermissionState
+import com.google.accompanist.permissions.shouldShowRationale
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,7 +27,30 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             PushReceiverSampleTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Column(
+                        modifier = Modifier
+                            .padding(innerPadding)
+                            .fillMaxSize()
+                            .padding(16.dp),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        RequestMmsPermission()
+                    }
+                }
             }
         }
+    }
+}
+
+@OptIn(ExperimentalPermissionsApi::class)
+@Composable
+fun RequestMmsPermission() {
+    val mmsPermissionState = rememberPermissionState(android.Manifest.permission.RECEIVE_MMS)
+    when {
+        mmsPermissionState.status.isGranted -> Text("MMS permission granted")
+        mmsPermissionState.status.shouldShowRationale -> Text("MMS permission denied")
+        else -> SideEffect { mmsPermissionState.launchPermissionRequest() }
     }
 }
